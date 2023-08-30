c8eae6ef0ed1da0e859e0f11c75fe713e14a88ea
kato1628 <t08265hk@gmail.com>
2023-08-30 21:50:32 +0100

    optimise parameters

--- evaluation_configuration.py
+++ evaluation_configuration.py
@@ -1,5 +1,5 @@
 eval_config = {
-    "filepath": "./dataset/wiki2018_valid_10000_60000.tr",
+    "filepath": "./dataset/wiki2018_valid_50000_100000.tr",
     "window_size": 550000,
     "capacity": 500000000,
     "access_history_len": 1,
