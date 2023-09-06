05e67e5816a128c8ceb703ac742bd02bf5a0a082
kato1628 <t08265hk@gmail.com>
2023-09-06 10:44:48 +0100

    plot experiment results

--- evaluation_configuration.py
+++ evaluation_configuration.py
@@ -1,7 +1,7 @@
 eval_config = {
-    "filepath": "./dataset/wiki2018_valid_50000_100000.tr",
+    "filepath": "./dataset/wiki2018_valid_200000_300000.tr",
     "window_size": 550000,
-    "capacity": 500000000,
+    "capacity": 2000000000,
     "access_history_len": 1,
     "scorer_type": "learned",
 }
